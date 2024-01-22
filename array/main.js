2e5b90c2a83648d9887f95cc06c4d3b984a3f3b8
Meycemabd <113856140+Meycemabd@users.noreply.github.com>
2024-01-22 21:00:13 +0100

    Work in Progress

--- array/main.js
+++ array/main.js
@@ -1,1 +1,10 @@
-let array = [1,2,3]
+// Exercises
+// Arrays || W3.D3.01
+// ESSENTIAL
+
+
+// 1.Write a function called arrayFor that takes an array as a parameter, loops through all the elements using For Loop and prints all elements of the array in the console using console.log.
+
+function arrayFor(array) {
+    
+}
