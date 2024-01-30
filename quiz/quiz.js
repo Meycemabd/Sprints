8ad4aea0394ea1f24992efc6ded3e46f1ba3ae54
Meycemabd <113856140+Meycemabd@users.noreply.github.com>
2024-01-30 19:24:10 +0100

    Doing Excercises

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -24,70 +24,77 @@
 //       character : "/assets/img/onePiece.png"
 //     },
 //   ];
- 
- let makeQuestions = (description,answer, point,coverImg,character) =>{
-  let obj={}
-  obj.description = description
-  obj.answer = answer
-  obj.point = point
-  obj.coverImg = coverImg
-  obj.character = character
-  return obj
- }
 
-let naruto = makeQuestions("A ninja with a dream of becoming the strongest ninja and leader of his village.","Naruto", 5,"https://i.pinimg.com/564x/85/31/7b/85317b621555591cfdc1daebd40f399d.jpg","https://photos.tf1.fr/780/780/perso-showpage-8efac4-fe1481-0@1x.png")
-let deathNote = makeQuestions("A high school student who gains the powers of a death god and uses them to bring justice.","Death Note", 10,"https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg","/assets/img/death.png")
-let onePiece = makeQuestions("A group of friends embark on an adventure to find a mysterious treasure.","One Piece", 2,"https://www.manga-news.com/public/images/pix/dvd/690/One-piece-anime-east-blue.jpg","/assets/img/onePiece.png")
+let makeQuestions = (description, answer, point, coverImg, character) => {
+  let obj = {};
+  obj.description = description;
+  obj.answer = answer;
+  obj.point = point;
+  obj.coverImg = coverImg;
+  obj.character = character;
+  return obj;
+};
 
+let naruto = makeQuestions(
+  "A ninja with a dream of becoming the strongest ninja and leader of his village.",
+  "Naruto",
+  5,
+  "https://i.pinimg.com/564x/85/31/7b/85317b621555591cfdc1daebd40f399d.jpg",
+  "https://photos.tf1.fr/780/780/perso-showpage-8efac4-fe1481-0@1x.png"
+);
+let deathNote = makeQuestions(
+  "A high school student who gains the powers of a death god and uses them to bring justice.",
+  "Death Note",
+  10,
+  "https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+  "./assets/img/death.png"
+);
+let onePiece = makeQuestions(
+  "A group of friends embark on an adventure to find a mysterious treasure.",
+  "One Piece",
+  2,
+  "https://www.manga-news.com/public/images/pix/dvd/690/One-piece-anime-east-blue.jpg",
+  "./assets/img/onePiece.png"
+);
 
-let quiz = [naruto, deathNote, onePiece]
+let quiz = [naruto, deathNote, onePiece];
 
+// let score = 0;
+// let index = 0;
 
+// document.querySelector("#question").innerText = quiz[index].description;
+// document.querySelector("#score").innerText = score;
+// document.querySelector("#cover").src = quiz[index].coverImg;
+// document.querySelector("#character").src = quiz[index].character;
 
+// function nextQuestion() {
+//   let answer = document.querySelector("#answer").value.toLowerCase();
 
-
-let score = 0;
-let index = 0;
-
-
-document.querySelector("#question").innerText = quiz[index].description;
-document.querySelector("#score").innerText = score;
-document.querySelector("#cover").src=quiz[index].coverImg
-document.querySelector("#character").src=quiz[index].character
-
-
-
-function nextQuestion() {
-    let answerInput = document.querySelector("#answer").value.toLowerCase();
-
-    if (answerInput === quiz[index].answer.toLowerCase()) {
-        score += quiz[index].point;
-        document.querySelector("#score").innerText = score;
-        document.querySelector("#cover").src=quiz[questionIndex].coverImg
-        document.querySelector("#character").src=quiz[questionIndex].character
-        document.querySelector("#yes").play();
-        index++;
-        if (index < quiz.length) {
-            document.querySelector("#question").innerText = quiz[index].description;
-        } else {
-            alert("Quiz completed! Your final score is: " + score);
-        }
-    } else {
-
-        alert("Wrong Answer! Try again");
-        document.querySelector("#gun").play();
-        document.querySelector("#youShD").play();
-    }
-}
-
-
+//   if (answer === quiz[index].answer.toLowerCase()) {
+//     document.querySelector("#yes").play();
+//     score += quiz[index].point;
+//     document.querySelector("#score").innerText = score;
+//     index++;
+//     document.querySelector("#answer").value = ""
+//     document.querySelector("#question").innerText = quiz[index].description;
+//     document.querySelector("#cover").src = quiz[index].coverImg;
+//     document.querySelector("#character").src = quiz[index].character;
+//   }
+//   else {
+//       alert("Wrong Answer! Try again");
+//       document.querySelector("#gun").play();
+//       document.querySelector("#youShD").play();
+//     }
+//     if (index === quiz.length) {
+//         alert("Quiz completed! Your final score is: " + score);F
+// }
+// }
 
 // let questionIndex = 0;
 
 // document.getElementById("question").innerText=quiz[questionIndex].description
 // document.querySelector("#cover").src=quiz[questionIndex].coverImg
 // document.querySelector("#character").src=quiz[questionIndex].character
-
 
 // let nextQuestion = () => {
 // questionIndex++
@@ -96,5 +103,15 @@ function nextQuestion() {
 // document.querySelector("#character").src=quiz[questionIndex].character
 // };
 
+$("#root").addClass("d-flex justify-content-around align-items-center");
+$("#root").css({ height: "100vh" });
+for (let i = 0; i < quiz.length; i++) {
+  $("#root").append(`
+<div>
+<h2>${quiz[i].answer}</h2>
+<img src=${quiz[i].coverImg} loading="lazy" width="300" height="300">
+</div>`);
+}
 
-
+$("#root").fadeIn(500);
+$("#root").fadeOut(500);
--- whileLoop/main.js
+++ whileLoop/main.js
@@ -3,108 +3,154 @@
 // 1.Write a function called sumOfN that takes a number as a
 // parameter and returns the sum of that number and all the numbers before it.
 
-
 function sumOfN(n) {
-    let result = 0
-    let i = 0;
-    while (i <= n) {
-        result += i
-        i++
-    }
-    return result
+  let result = 0;
+  let i = 0;
+  while (i <= n) {
+    result += i;
+    i++;
+  }
+  return result;
 }
 
-//2.Write a function called factorialOfN that takes a whole number 
+//2.Write a function called factorialOfN that takes a whole number
 //as a parameter and returns the factorial of that number.
 
 function factorialOfN(n) {
-    let result = 1
-    let i = 1;
-    while (i <= n) {
-        result *= i
-        i++
-    }
-    return result
+  let result = 1;
+  let i = 1;
+  while (i <= n) {
+    result *= i;
+    i++;
+  }
+  return result;
 }
 
-
-// 3.Write a function called repeatString that takes two parameters, as string and a number, and returns 
+// 3.Write a function called repeatString that takes two parameters, as string and a number, and returns
 //that string the number of times specified in the second parameter.
 
 function repeatString(string, num) {
-    let result = ""
-    let i = 1
-    while (i <= num) {
-        result = result + string;
-        i = i + 1
-    }
-    return result
-} 
+  let result = "";
+  let i = 1;
+  while (i <= num) {
+    result = result + string;
+    i = i + 1;
+  }
+  return result;
+}
 
-// 4.Write a function called countMinMax 
+// 4.Write a function called countMinMax
 //that takes two numbers as parameters and returns the range.
 
 function countMinMax(min, max) {
-   let i = 0 
-   while(min<max){
-    i++
-    min++
-   }
-   return i 
- } 
+  let i = 0;
+  while (min < max) {
+    i++;
+    min++;
+  }
+  return i;
+}
 
-//  5.Write a function called sumMinToMax that takes two numbers as parameters and returns the sum 
+//  5.Write a function called sumMinToMax that takes two numbers as parameters and returns the sum
 //between the two integers beginning at the num1 and excluding num2.
 
 function sumMinToMax(min, max) {
-   let result = 0 
-    let i = min ; 
-    while(i<max){
-        result += i 
-        i++
-    }
-   return result
- }  
+  let result = 0;
+  let i = min;
+  while (i < max) {
+    result += i;
+    i++;
+  }
+  return result;
+}
 
-//6.Write a function called productMinToMax that takes two numbers as parameters and returns the product 
+//6.Write a function called productMinToMax that takes two numbers as parameters and returns the product
 //between the two integers beginning at the num1 and excluding num2.
 
 function productMinToMax(min, max) {
-    let result = min 
-    let i = 1 
-    while(i<max){
-        result *= i
-        i++
+  let result = min;
+  let i = 1;
+  while (i < max) {
+    result *= i;
+    i++;
+  }
+  return result;
+}
+
+//7.Write a function called multiplyBy10NTimes that takes two numbers as parameters and returns the first number multiplied
+//by 10 the amount of times indicated by the second number.
+
+function multiplyBy10NTimes(num, n) {
+  let i = 1;
+  while (i <= n) {
+    num *= 10;
+    i++;
+  }
+  return num;
+}
+
+//8.Write a function called countCharAtIndex that takes three parameters, a string, an index, and another string. This function should use the index to find the corresponding character in the first string and loop through
+//the second string and count
+//how many times that character occurs.
+
+function countCharAtIndex(string1, index, string2) {
+  let count = 0;
+  let i = 0;
+  while (i < string2.length) {
+    if (string1.at(index) === string2.at(i)) {
+      // if the character of string1 at index is equal to stiring2 at index of "i"
+
+      count++;
     }
-    return result
- } 
+    i++;
+    return count;
+  }
+}
 
- //7.Write a function called multiplyBy10NTimes that takes two numbers as parameters and returns the first number multiplied 
- //by 10 the amount of times indicated by the second number.
+// EXTENTED
+// 1.Write a function called reverseString that takes a string as an input and returns that string in reverse.
 
- function multiplyBy10NTimes(num, n) {
-    let i = 1 
-    while(i<= n) {
-        num *= 10
-        i++
+function reverseString(string) {
+  let reversedString = "";
+  let i = string.length - 1;
+  while (i >= 0) {
+    reversedString += string[i];
+    i--;
+  }
+  return reversedString;
+}
+
+// 2.Write a function called getIndexOf that takes two parameters, a string, and a character, and returns the first position of the character in that string.
+
+function getIndex(string, char) {
+  let i = 0;
+  while (i <= string.length) {
+    if (string[i] === char) {
+      return i;
     }
-    return num
- } 
+    i++;
+  }
+  return "charachter Not found";
+}
 
+// IMMERSIVE
+// 1.Write a function called sumEven that takes two numbers as parameters and returns the sum of all even numbers starting from num1 and excluding num2.
 
- //8.Write a function called countCharAtIndex that takes three parameters, a string, an index, and another string. This function should use the index to find the corresponding character in the first string and loop through 
- //the second string and count 
- //how many times that character occurs.
-
- function countCharAtIndex(string1, index, string2) {
-    let count = 0; // counter 
-    let i = 0  // the iterator 
-    while(i<string2.length){ // stoping condition 
-        if(string1.at(index) === string2.at(i) ) { // if the character of string1 at index is equal to stiring2 at index of "i"
-
-            count++ // than counter ++ 
-        }
-        i++ // increamenting the iterator to reach the stoping condition 
+function sumEven(num1, num2) {
+  let result = 0;
+  let i = num1;
+  while (i < num2) {
+    if (i % 2 === 0) {
+      result += i;
     }
-    return count  // returning the counter
- } 
+    i++;
+  }
+  return result;
+}
+
+
+// 2.Write a function called primeCounter that takes a number as a parameter and returns the amount of prime numbers that occur before it.
+
+function primeCounter(number) {
+    //Write your code here 
+ }  
